6bb0ca010510a63979dffa85aea5bc422c98ea60
Thales <thales@carteiraglobal.com>
2019-10-26 01:08:34 -0300

    Setup inicial, font face + Lerolero component

---
+++ src/components/Lerolero/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./lerolero.module.css";
+
+const Lerolero = () => {
+  return (
+    <article className={styles.container}>
+      <blockquote>Imagination is more important than knowledge</blockquote>
+    </article>
+  );
+};
+
+export default Lerolero;
